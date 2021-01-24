25d6dda859cc369e1339a180d118f9a23c9996cc
Robertas Liekis <robertasliekis@gmail.com>
2021-01-24 14:17:04 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./css/App.module.css";
 
-function App() {
+import StationsWidget from "./components/StationsWidget";
+
+//"homepage": "https://robertasliekis.github.io/station-widget/",
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <StationsWidget />
     </div>
   );
-}
+};
 
 export default App;
